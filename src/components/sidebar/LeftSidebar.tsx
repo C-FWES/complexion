20f170d8e0bbe7c7d9ff8fe9c26559e1de513334
C-FWES <353861933@educ.dpcdsb.org>
2024-05-14 19:38:51 -0400

    ui: sidebar slide preview layout

--- src/components/sidebar/LeftSidebar.tsx
+++ src/components/sidebar/LeftSidebar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useState } from 'react'
+import PreviewSlide from "../slide/PreviewSlide";
 
 function LSidebar() {
   
     return (
       <>
         <div className='leftSidebarContainer'>
-         
+            <PreviewSlide></PreviewSlide>
+            <PreviewSlide></PreviewSlide>
+            <PreviewSlide></PreviewSlide>
         </div>
       </>
     )
---
+++ src/components/slide/PreviewSlide.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from 'react'
+
+function PreviewSlide() {
+  
+    return (
+      <>
+        <div className='previewSlideContainer'>
+         
+        </div>
+      </>
+    )
+  }
+  
+  export default PreviewSlide
