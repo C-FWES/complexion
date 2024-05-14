436eadac27f26a4f73727ef9d672d9e751ef5d90
C-FWES <353861933@educ.dpcdsb.org>
2024-05-14 18:42:40 -0400

    ui: sidebar layout

---
+++ src/components/sidebar/LeftSidebar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from 'react'
+
+function LSidebar() {
+  
+    return (
+      <>
+        <div className='leftSidebarContainer'>
+         
+        </div>
+      </>
+    )
+  }
+  
+  export default LSidebar
