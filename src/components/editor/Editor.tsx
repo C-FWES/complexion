a0702a962de8151433958f990e7d17d2a0bd8043
C-FWES <353861933@educ.dpcdsb.org>
2024-05-14 19:59:36 -0400

    create editor component

---
+++ src/components/editor/Editor.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from 'react'
+
+function Editor() {
+  
+    return (
+      <>
+        <div className='editorContainer'>
+         
+        </div>
+      </>
+    )
+  }
+  
+  export default Editor
