160dcbc2b3e423ee3688323dfbc1564c64357894
C-FWES <353861933@educ.dpcdsb.org>
2024-05-18 16:45:30 -0400

    assign text to slide state

--- src/components/sidebar/LeftSidebar.tsx
+++ src/components/sidebar/LeftSidebar.tsx
@@ -5,12 +5,12 @@ import PreviewSlide from "../slide/PreviewSlide";
 function LSidebar() {
 
     const [createdSlides, setCreatedSlides] = useState([])
-    // console.log(createdSlides)
+    console.log(createdSlides)
 
     useEffect(() => {
       const handleDeploySlide = (event) => {
         // create an empty slide object
-        const slideObj = [event.detail, "empty"]
+        const slideObj = [event.detail, ""]
         setCreatedSlides(prevSlides => [...prevSlides, slideObj])
       }
 
@@ -23,14 +23,34 @@ function LSidebar() {
         }
       }
 
+      const updateSlideContent = (event) => {
+        const slideIndex = event.detail[0]
+        console.log("slide index " + slideIndex)
+        const contentObject = event.detail[1]
+        console.log("content to " + contentObject)
+        // update content here
+        setCreatedSlides(prevSlides => {
+          return prevSlides.map((slide, index) => {
+            if (slide[0] === slideIndex && contentObject.length > 0) {
+              console.log('good!')
+              return [slide[0], contentObject]; // update the second index with contentObject
+            } else {
+              return slide; // return the slide unchanged
+            }
+          });
+        });
+      }
+
       window.addEventListener('deploySlide', handleDeploySlide)
       window.addEventListener('removeSlide', handleRemoveSlide)
+      window.addEventListener('updateSlide', updateSlideContent)
 
       return () => {
         window.removeEventListener('deploySlide', handleDeploySlide)
         window.removeEventListener('removeSlide', handleRemoveSlide)
+        window.removeEventListener('updateSlide', updateSlideContent)
       }
-    }, [])
+    }, [createdSlides])
   
     return (
       <>
