a19170dd1b134cd378b3f05dfa698708ac691302
C-FWES <353861933@educ.dpcdsb.org>
2024-05-18 17:05:00 -0400

    display the plaintext in slide - needs md parsing

--- src/components/slide/PreviewSlide.tsx
+++ src/components/slide/PreviewSlide.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from 'react'
 
 function PreviewSlide({slideInfo}) {
@@ -6,12 +6,16 @@ function PreviewSlide({slideInfo}) {
     const [header, setHeader] = useState(null)
     const [subHeader, setSubheader] = useState(null)
     const [ol, setOl] = useState(null)
+
+    useEffect(() => {
+      setHeader(slideInfo[1])
+    }, [slideInfo])
   
     return (
       <>
         <div className='previewSlideContainer'>
           <div className="previewSlideTextContainer">
-            
+            {header && <h1>{header}</h1>}
           </div>
         </div>
       </>
