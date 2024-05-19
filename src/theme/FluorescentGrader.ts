dfe6e7e6f0512af5805a0c25c46b455ad788c5e2
C-FWES <353861933@educ.dpcdsb.org>
2024-05-19 13:35:57 -0400

    improve color range

--- src/theme/FluorescentGrader.ts
+++ src/theme/FluorescentGrader.ts
@@ -1,8 +1,8 @@
 // dynamically determines color of slide based on first slide color and slide index
-const initialHsl = "hsl(190, 100%, 50%)"
+const initialHsl = "hsl(190, 100%, 45%)"
 
 export const gradeSlideFluorescentHsl = (i) => {
-    const hslOrientation = 190 + (i * 10);
+    const hslOrientation = 190 + (i * 20);
     console.log(hslOrientation)
     return `hsl(${hslOrientation}, 100%, 50%)`;
 }
