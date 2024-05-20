82d2e3c209b32c8eae8c67e7d93454c5500fcc47
C-FWES <353861933@educ.dpcdsb.org>
2024-05-20 15:28:37 -0400

    remove '#' symbols from headers

--- src/components/slide/PreviewSlide.tsx
+++ src/components/slide/PreviewSlide.tsx
@@ -1,16 +1,30 @@
 import React, { useEffect } from "react";
 import { useState } from 'react'
 import { gradeSlideFluorescentHsl } from "../../theme/FluorescentGrader";
+import { parsePrefix } from "../../parse/ParsePrefix";
 
 function PreviewSlide({slideInfo, index}) {
 
     const [header, setHeader] = useState(null)
     const [subHeader, setSubheader] = useState(null)
+    const [subsubHeader, setSubsubheader] = useState(null)
     const [ol, setOl] = useState(null)
 
-    // useEffect(() => {
-    //   setHeader(slideInfo[1][0])
-    // }, [slideInfo])
+    useEffect(() => {
+      // parse markdown strings
+      for (const stem of slideInfo[1]) {
+        if (stem && stem.startsWith('#')) {
+          const headerType = parsePrefix(stem)
+          if (headerType === 'h1') {
+            setHeader(stem.substring(2))
+          } else if (headerType === 'h2') {
+            setSubheader(stem.substring(3))
+          } else if (headerType === 'h3') {
+            setSubsubheader(stem.substring(4))
+          }
+        }
+      }
+    }, [slideInfo])
 
     const slideBgHsl = gradeSlideFluorescentHsl(index);
   
